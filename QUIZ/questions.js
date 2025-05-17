c5c475b44bf0c7164475414c2b6d8e84fa6d0d59
LUIS FERNANDO LIRA COELHO <fernandolirac2005@gmail.com>
2025-05-17 17:24:12 -0300

    Nova NavBar

---
+++ QUIZ/questions.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    question: "O que é energia solar?",
+    answers: [
+      { option: "A energia gerada pelos ventos.", correct: false },
+      { option: "A energia proveniente da água.", correct: false },
+      { option: "A energia gerada pela luz e calor do Sol.", correct: true },
+      { option: "A energia criada a partir de minerais.", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a principal fonte da energia solar?",
+    answers: [
+      { option: "Eles transformam o vento em energia elétrica.", correct: false },
+      { option: "Eles armazenam energia térmica e liberam eletricidade.", correct: true },
+      { option: "Eles usam a luz solar para gerar eletricidade através do efeito fotovoltaico.", correct: false },
+      { option: "Eles convertem calor do solo em eletricidade.", correct: true },
+    ],
+  },
+  {
+    question: "Como os painéis solares geram eletricidade?",
+    answers: [
+      { option: "Eles transformam o vento em energia elétrica.", correct: false },
+      { option: "90", correct: false },
+      { option: "80", correct: true },
+      { option: "49", correct: true },
+    ],
+  },
+  {
+    question: "A energia solar é renovável ou não renovável?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+      { option: "49", correct: true },
+    ],
+  },
+  {
+    question: "Qual é o principal benefício da energia solar para o meio ambiente?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+      { option: "49", correct: true },
+    ],
+  },
+  {
+    question: "Onde os painéis solares são normalmente instalados?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+      { option: "49", correct: true },
+    ],
+  },
+  {
+    question: "A energia solar pode ser utilizada durante a noite? Por quê?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+      { option: "49", correct: true },
+    ],
+  },
+  {
+    question: "Qual país tem grande produção de energia solar?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+      { option: "49", correct: true },
+    ],
+  },
+  {
+    question: "O que acontece com a energia extra gerada por um painel solar?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+      { option: "49", correct: true },
+    ],
+  },
+  {
+    question: "Além da eletricidade, para que mais a energia solar pode ser usada?",
+    answers: [
+      { option: "70", correct: false },
+      { option: "56", correct: false },
+      { option: "49", correct: true },
+      { option: "49", correct: true },
+    ],
+  },
+];
