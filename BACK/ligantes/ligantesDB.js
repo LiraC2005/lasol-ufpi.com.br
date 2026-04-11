181c6bc9a837b466aa26dd3ca88c79fc33d54a72
LUIS FERNANDO LIRA COELHO <fernandolirac2005@gmail.com>
2026-04-11 14:53:49 -0300

    atualização de cargos

--- BACK/ligantes/ligantesDB.js
+++ BACK/ligantes/ligantesDB.js
@@ -48,57 +48,37 @@ export const membros = {
       link: "https://www.linkedin.com/in/israel-soares-4058a8189?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
     },
 
-    {
-      nome: "Maxwell Cardoso",
-      cargo: "Membro",
-      foto: "./asset/ligantes/MAX.webp",
-      link: "https://www.instagram.com/maxwell_cardoso777/?utm_source=ig_web_button_share_sheet"
-    },
 
-
-    {
-      nome: "Nicolas Nilton",
-      cargo: "Membro",
-      foto: "./asset/ligantes/ligante22.webp",
-      link: "#"
-    },
-
-
-    {
-      nome: "Jailson Iran",
-      cargo: "Membro",
-      foto: "./asset/ligantes/jailson.webp",
-      link: "https://www.instagram.com/jailsoniran/?utm_source=ig_web_button_share_sheet"
-    },
 
   ],
   diretoriaPesquisa: [
     {
-      nome: "Victor Gabriel",
+      nome: "Klemeson Lucas",
       cargo: "Diretor",
-      foto: "./asset/ligantes/Victor.webp",
-      link: "https://www.instagram.com/vgabriel.rc?utm_source=ig_web_button_share_sheet&igsh=YWd1dGM5b2Q1a3lm"
+      foto: "./asset/ligantes/Klemeson.webp",
+      link: "https://www.linkedin.com/in/klemesonneves?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
     },
+    
     {
       nome: "Maria Manuela",
       cargo: "Vice-Diretora",
       foto: "./asset/ligantes/Manuela.webp",
       link: "https://www.instagram.com/maria12_manuela?utm_source=ig_web_button_share_sheet&igsh=Z2h0ODZwZjg3ZGp5"
     },
+
+    {
+      nome: "Victor Gabriel",
+      cargo: "Membro",
+      foto: "./asset/ligantes/Victor.webp",
+      link: "https://www.instagram.com/vgabriel.rc?utm_source=ig_web_button_share_sheet&igsh=YWd1dGM5b2Q1a3lm"
+    },
+
     {
       nome: "Italo Abreu",
       cargo: "Membro",
       foto: "./asset/ligantes/Italo.webp",
       link: "https://www.instagram.com/it_abreu/?utm_source=ig_web_button_share_sheet"
     },
-    {
-      nome: "Jonathan Vinícius",
-      cargo: "Membro",
-      foto: "./asset/ligantes/Exemplar.webp",
-      link: "https://www.instagram.com/jonathanvasilva/?utm_source=ig_web_button_share_sheet"
-    },
-
-
 
     {
       nome: "Ana Karoline",
@@ -116,16 +96,16 @@ export const membros = {
   ],
   diretoriaExtensao: [
     {
-      nome: "Naywan Raphael",
-      cargo: "Diretor",
-      foto: "./asset/ligantes/Naywan.webp",
-      link: "https://www.instagram.com/naywan_raphael/?utm_source=ig_web_button_share_sheet"
-    },
-    {
       nome: "Maria Fernanda",
-      cargo: "Vice-Diretora",
+      cargo: "Diretora",
       foto: "./asset/ligantes/Fernanda.webp",
       link: "https://www.instagram.com/mariaferndx/?utm_source=ig_web_button_share_sheet"
+    },
+    {
+      nome: "Naywan Raphael",
+      cargo: "vice-Diretor",
+      foto: "./asset/ligantes/Naywan.webp",
+      link: "https://www.instagram.com/naywan_raphael/?utm_source=ig_web_button_share_sheet"
     },
     {
       nome: "Lucas B. Diniz",
@@ -133,24 +113,14 @@ export const membros = {
       foto: "./asset/ligantes/LUCASBD.webp",
       link: "https://www.linkedin.com/in/lucasbarbosadiniz"
     },
-    {
-      nome: "Klemeson Lucas",
-      cargo: "Membro",
-      foto: "./asset/ligantes/Klemeson.webp",
-      link: "https://www.linkedin.com/in/klemesonneves?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-    },
+    
     {
       nome: "Matheus Carvalho",
       cargo: "Membro",
       foto: "./asset/ligantes/matheuscarvalho.webp",
       link: "https://www.linkedin.com/in/matheus-silva-de-carvalho-133795257?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
     },
-    {
-      nome: "Antônio Gustavo",
-      cargo: "Membro",
-      foto: "./asset/ligantes/Gustavo.webp",
-      link: "https://www.instagram.com/gustavosmoura_?utm_source=ig_web_button_share_sheet&igsh=b3Z3Z2c1aWhnY2lk"
-    },
+    
     {
       nome: "João Marcelo",
       cargo: "Membro",
@@ -162,16 +132,22 @@ export const membros = {
   ],
   diretoriaComunicacao: [
     {
-      nome: "Clara Harijan",
-      cargo: "Diretora",
-      foto: "./asset/ligantes/Clara.webp",
-      link: "https://www.instagram.com/clara_harijan?utm_source=ig_web_button_share_sheet&igsh=bGo5ZXFxMWc4Njdt"
+      nome: "Caio Eros",
+      cargo: "Diretor",
+      foto: "./asset/ligantes/Eros.webp",
+      link: "https://www.instagram.com/ersx.lima/?utm_source=ig_web_button_share_sheet"
     },
     {
       nome: "Matheus Leite",
       cargo: "Vice-Diretor",
       foto: "./asset/ligantes/Matheus.webp",
       link: "https://www.linkedin.com/in/mateus-rodrigues-de-ara%C3%BAjo-leite-972251217?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+    },
+    {
+      nome: "Clara Harijan",
+      cargo: "Membro",
+      foto: "./asset/ligantes/Clara.webp",
+      link: "https://www.instagram.com/clara_harijan?utm_source=ig_web_button_share_sheet&igsh=bGo5ZXFxMWc4Njdt"
     },
     {
       nome: "Samuel",
@@ -200,18 +176,14 @@ export const membros = {
       foto: "./asset/ligantes/Erasmo.webp",
       link: "#"
     },
+    
     {
       nome: "Caio Eros",
       cargo: "Membro",
       foto: "./asset/ligantes/Eros.webp",
       link: "https://www.instagram.com/ersx.lima/?utm_source=ig_web_button_share_sheet"
     },
-    {
-      nome: "Luís Sílvio",
-      cargo: "Membro",
-      foto: "./asset/ligantes/LS.webp",
-      link: "#"
-    },
+   
     {
       nome: "Antoniel Xavier",
       cargo: "Membro",
